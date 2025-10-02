62fb99239c959920286051d7b314c116fcb364b4
Andrii Unhurian <andrii.unhurian@sapient.pro>
2025-10-02 08:28:13 +0300

    Update IntelliJ version in build.gradle.kts to 2025.2.2 for compatibility improvements.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("PS-2025.1.3")
+    version.set("PS-2025.2.2")
     type.set("IU")
     plugins.set(listOf("com.jetbrains.php", "com.intellij.modules.json"))
 }
